eecdd21d1fb1d0360051301c3fc3fcbc25b0bb86
sophanna <sophanna.ly88@gmail.com>
2022-04-23 22:30:19 +1000

    fuctions add, minus and calculate total amount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { Display } from "./components/display/Display";
 import { Info } from "./components/info/Info";
 import { Top } from "./components/navbar/Top";
 
+
 function App() {
+  const [totalAmount, setTotalAmount] = useState(125);
+  
+
+  const moneyAdd=()=>{
+    console.log("clicked");
+    setTotalAmount(prev => prev + 125);
+    // console.log(totalAmount);
+  }
+  // console.log(totalAmount);
+
+  const moneyMinus=()=>{
+    console.log("minus now");
+    setTotalAmount(prev => prev - 125);
+  }
   return (
+  
     <div className="wrapper">
       <Container>
         <Top />
@@ -15,7 +32,7 @@ function App() {
           </Col>
 
           <Col md="6">
-            <Info />
+            <Info moneyAdd={moneyAdd} totalAmount={totalAmount} moneyMinus={moneyMinus}/>
           </Col>
         </Row>
       </Container>
--- src/components/quantityButton/QuantityButton.js
+++ src/components/quantityButton/QuantityButton.js
@@ -1,41 +1,49 @@
 import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 import { useState } from "react";
 
-export const QuantityButton = () => {
-  const [value, setValue] = useState([1, 3]);
-
+export const QuantityButton = ({moneyAdd, moneyMinus}) => {
+  const [value, setValue] = useState(1);
+  const add =() =>{
+    setValue(value + 1);
+    moneyAdd();
+  }
+  const minus =()=>{
+    if(value>1){
+      setValue(value -1);
+      moneyMinus();
+    }
+  }
   /*
    * The second argument that will be passed to
    * `handleChange` from `ToggleButtonGroup`
    * is the SyntheticEvent object, but we are
    * not using it in this example so we will omit it.
    */
-  const handleChange = (val) => setValue(val);
+  
 
   return (
     <ToggleButtonGroup
       className="me-3  "
       type="checkbox"
-      value={value}
-      onChange={handleChange}
+      
       style={{ maxHeight: "48px" }}
     >
       <ToggleButton
         id="tbg-btn-1"
-        value={1}
+        
         className="fw-bolder fs-4"
         style={{
           backgroundColor: "rgb(240, 240, 240)",
           borderColor: "lightgray",
           color: "hsl(26, 100%, 55%)",
         }}
-      >
+        onClick={minus}>
         -
       </ToggleButton>
 
       <ToggleButton
         id="tbg-btn-2"
-        value={2}
+        
         className="fw-bolder fs-6 "
         style={{
           backgroundColor: "rgb(240, 240, 240)",
@@ -43,19 +51,19 @@ export const QuantityButton = () => {
           color: "black",
         }}
       >
-        0
+        {value}
       </ToggleButton>
 
       <ToggleButton
         id="tbg-btn-3"
-        value={3}
+        
         className="fw-bolder fs-4"
         style={{
           backgroundColor: "rgb(240, 240, 240)",
           borderColor: "lightgray",
           color: "hsl(26, 100%, 55%)",
         }}
-      >
+       onClick={add} >
         +
       </ToggleButton>
     </ToggleButtonGroup>
